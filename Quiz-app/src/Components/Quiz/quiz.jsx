db77c87f1eb6bd80f3706b6c9a77807cff135fe1
NathanelPrah <NathanaelPrah@gmail.com>
2024-04-22 02:29:51 +0000

    changes

--- Quiz-app/src/Components/Quiz/quiz.jsx
+++ Quiz-app/src/Components/Quiz/quiz.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { data } from '../../assets/data';
+
 import './Quiz.css'
 const quiz = () => {
+
+
+    let [index,setIndex] = useState(1);
+    let [question,setQuestion] = useState(data[index]);
+
+
   return (
     <div className='container'>
         <h1> Quiz App</h1>
         <hr/>
-        <h2>What the Name of your country?</h2>
+        <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li>Ghana</li>
-            <li>Togo</li>
-            <li>China</li>
-            <li>Canada</li>
+            <li>{question.option1}</li>
+            <li>{question.option2}</li>
+            <li>{question.option3}</li>
+            <li>{question.option4}</li>
+
+          
             <button>Next</button>
 
             <div className='index'> 1 0f 20 questions</div>
