b351afd16d84154e6794462a5e73fd9363aedca6
NathanelPrah <NathanaelPrah@gmail.com>
2024-04-22 01:26:21 +0000

    changes

---
+++ Quiz-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import quiz from './Components/Quiz/quiz'
+const App = () => {
+  return (
+    <>
+      <quiz/>
+
+    </>
+  )
+}
+
+export default App
---
+++ Quiz-app/src/Components/Quiz/quiz.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './quiz.css'
+const quiz = () => {
+  return (
+    <div className='container'>
+        <h1> Quiz App</h1>
+        <hr/>
+
+</div>
+  )
+}
+
+export default quiz
