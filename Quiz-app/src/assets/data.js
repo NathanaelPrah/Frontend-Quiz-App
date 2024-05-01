3403c704e2d4ece044b4d2aa465f3837b2a32f36
PrahNathanael <NathanaelPrah@gmail.com>
2024-05-01 14:06:59 +0000

    changes

--- Quiz-app/src/assets/data.js
+++ Quiz-app/src/assets/data.js
@@ -39,4 +39,51 @@ export const data = [
       option4: "Pen Drive",
       ans: 2,
     },
+     {
+        question: 'What does CSS stand for?',
+        option1:'Computer Style Sheets',
+        option2: 'Creative Style Sheets',
+        option3: 'Cascading Style Sheets',
+        option4: 'Colorful Style Sheets',
+        ans: 3
+    },
+
+    {
+        question: 'Where in an HTML document is the correct place to refer to an external style sheet?',
+        option1:"In the <head> section",
+        option2:"In the <body> section",
+        option3:"At the end of the document",
+        option4:"You can\'t refer to an external style sheet",
+        ans: 1
+    },    
+    {
+        question: 'Which HTML tag is used to define an internal style sheet?',
+        answers: ['<style>', '<script>', '<headStyle>', '<css>'],
+        correct: 1
+    },    
+    {
+        question: 'Which HTML attribute is used to define inline styles?',
+        answers: ['class', 'style', 'font', 'styles'],
+        correct: 2
+    },
+    {
+        question: 'Which is the correct CSS syntax?',
+        answers: ['{body:color=black;}', '{body;color:black;}', 'body:color=black;', 'body {color: black;}'],
+        correct: 4
+    },
+    {
+        question: 'How do you insert a comment in a CSS file?',
+        answers: ['\' this is a comment', '/* this is a comment */', '// this is a comment', '// this is a comment //'],
+        correct: 2
+    },
+    {
+        question: 'Which property is used to change the background color?',
+        answers: ['color', 'bgcolor', 'background-color', 'bgColor'],
+        correct: 3
+    },
+    {
+        question: 'How do you add a background color for all <h1> elements?',
+        answers: ['all.h1 {background-color:#FFFFFF;}', 'h1.setAll {background-color:#FFFFFF;}', 'h1.all {background-color:#FFFFFF;}', 'h1 {background-color:#FFFFFF;}'],
+        correct: 4
+    }
   ];
